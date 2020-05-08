19e97bd5397d21354198933c8888e896b1af0d51
dkaznacheev <dmitry.kaznacheev@gmail.com>
2020-05-08 17:08:43 +0300

    reworked serialization in writing to file

--- src/main/kotlin/api/operations/SaveAsObjectOperation.kt
+++ src/main/kotlin/api/operations/SaveAsObjectOperation.kt
@@ -30,8 +30,6 @@ class SaveAsObjectOperation<T>(rdd: RDD<T>, val name: String): ParallelOperation
 }
 
 class SaveAsObjectOperationImpl<T>(rdd: RDDImpl<T>, val name: String): ParallelOperationImpl<T, Byte>(rdd) {
-
-
     @KtorExperimentalAPI
     override suspend fun execute(scope: CoroutineScope, ctx: WorkerContext): Byte {
         val recChannel = rdd.channel(scope, ctx)
